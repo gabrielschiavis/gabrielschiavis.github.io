afdb9bff07926a462bdfffa2016d1261b04dab37
GabrielSchiavi <133630923+gabrielschiavis@users.noreply.github.com>
2024-01-18 01:13:58 -0400

    Update ghae.js

--- ghae.js
+++ ghae.js
@@ -1,5 +1,6 @@
 function countText() {
   var elemento = document.querySelector('.texto');
   var conteudo = elemento.textContent || elemento.innerText; 
+  alert(conteudo.length);
   document.getElementById('words').innerText = conteudo.length == 0 ? 0 : conteudo.split(/\s+/).length;
 }
